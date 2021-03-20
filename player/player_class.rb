57477422d8ad8edced58c90bc895e6ee4acdf192
trallerd <jessicacristec@gmail.com>
2021-03-20 14:37:39 -0300

    testing player's classes

---
+++ player/player_class.rb
@@ -0,0 +1,8 @@
+class PlayerClass
+  attr_accessor :name, :craft
+
+  def initialize(name, craft)
+    @name = name
+    @craft = craft
+  end
+end
---
+++ test_class.rb
@@ -0,0 +1,41 @@
+require_relative 'player/player_class'
+require_relative 'breeds/human'
+
+human = Human.new
+
+puts('Class Name:')
+name = gets
+
+puts('What the class craft?')
+craft = gets
+
+puts('Lets see the modifiers:')
+
+puts('Life')
+life = gets.strip.to_i
+
+puts('Attack')
+attack = gets.strip.to_i
+
+puts('Defense')
+defense = gets.strip.to_i
+
+puts('Intelligence')
+intelligence = gets.strip.to_i
+
+puts('Strength')
+strength = gets.strip.to_i
+
+player_class = PlayerClass.new(name, craft)
+
+puts human.inspect
+
+human.life[0] += life
+human.attack += attack
+human.defense += defense
+human.intelligence += intelligence
+human.strength += strength
+
+puts human.inspect
+
+puts player_class.inspect
